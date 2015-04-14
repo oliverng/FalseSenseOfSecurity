5f83dcaa6ea0ceab3caa6da7eed2b3304a064c5e
Oliver N <oliverng@gmail.com>
2015-04-14 15:57:09 +0800

    Final version of iOS Debugging verification test application.

--- FalseSenseOfSecurity/ViewController.h
+++ FalseSenseOfSecurity/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *debugTxtLabel;
+@property (weak, nonatomic) IBOutlet UILabel *debugTest2TxtLabel;
 
 @end
 
--- FalseSenseOfSecurity/ViewController.m
+++ FalseSenseOfSecurity/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <sys/ptrace.h>
+#import "DebugValidate.h"     // import our Debugger Checking Static code provided by Apple Docs
 
 @interface ViewController ()
 
@@ -17,7 +19,48 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+ 
+  
+  // call our two debug detection routines
+  [self ptraceTest];
+  [self debuggerDetect];
 }
+
+- (void)ptraceTest {
+  
+  // check if app being debugged using the C macro
+#ifndef DEBUG
+  // if so, deny ptrace from attaching (will exit segfault gdb)
+  ptrace(PT_DENY_ATTACH, 0, 0, 0);
+#endif
+
+  // if we pass our test, update the label
+  self.debugTxtLabel.text = @"I passed ptrace test";
+  
+}
+
+- (void)debuggerDetect{
+  
+  // check if app being debugged using the C macro
+#ifndef DEBUG
+  // this code uses Apple's debugger detection
+  bool debugDetect = AmIBeingDebugged();
+  
+  // if debugger detected, update the label and return from function
+  if (debugDetect){
+    NSLog(@"Debugger detected, exit method.");
+    self.debugTest2TxtLabel.text = @"I am being debugged, Argh!";
+    return;
+  }
+#endif
+  
+  // passed debugger test, update the label - wont get here if above returned
+  self.debugTest2TxtLabel.text = @"I passed debugger detection";
+  
+}
+
+
+
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
